726b0ced5560bacff6e85997a00a08b75d9708e8
Nastya Tymoshenko <nastyatymoshenko7451@gmail.com>
2026-08-20 19:33:22 +0300

    Rotate Array solution 2 (19)

--- RotateArray.py
+++ RotateArray.py
@@ -4,3 +4,24 @@ class Solution:
         for i in range(len(nums)):
             result[i+k%len(nums)-len(nums)]=nums[i]
         nums[:]=result
+
+#second solution
+k=k%len(nums)
+nums.reverse()
+nums[:k]=reversed(nums[:k])
+nums[k:]=reversed(nums[k:])
+
+#or by using two pointers
+k=k%len(nums)
+l,r=0, len(nums)-1
+while l<r:
+    nums[l], nums[r]= nums[r], nums[l]
+    l,r=l+1, r-1
+l,r=0, k-1
+while l<r:
+    nums[l], nums[r]= nums[r], nums[l]
+    l,r=l+1, r-1
+l,r=k, len(nums)-1
+while l<r:
+    nums[l], nums[r]= nums[r], nums[l]
+    l,r=l+1, r-1
